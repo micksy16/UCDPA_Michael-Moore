6f1c14394b2887c0aceca67671f0ea8278e53fbe
Micksy16 <micksymooresy16@yahoo.com>
2021-07-22 12:24:10 +0100

    Added Columns, sorted data, merged csv's, dropped rows with no data, cleansed data.

--- main.py
+++ main.py
@@ -40,3 +40,60 @@ df = pd.read_csv("GOOG.csv")
 df1 = pd.read_csv("GME_stock.csv")
 df2 = pd.read_csv("Amazon.csv")
 
+# MM - Adding Unique Identifier Column
+
+df.insert(1, "Company", "Google")
+df1.insert(1, "Company", "Gamestop")
+df2.insert(1, "Company", "Amazon")
+
+# MM - sorting imported data
+
+df['Date'] = pd.to_datetime(df.Date, infer_datetime_format=True)
+df.sort_values(by='Date', ascending=False, inplace=True)
+print(df)
+
+df1['Date'] = pd.to_datetime(df1.Date, infer_datetime_format=True)
+df1.sort_values(by='Date', ascending=False, inplace=True)
+print(df1)
+
+df2['Date'] = pd.to_datetime(df2.Date, infer_datetime_format=True)
+df2.sort_values(by='Date', ascending=False, inplace=True)
+print(df2)
+
+# MM - merging csv's, first merge
+
+df3 = df.append(df1, sort=False)
+print(df3)
+
+# MM - merging csv's, second merge
+
+df4 = df3.append(df2, sort=False)
+print(df4)
+
+# MM - sorting merged file
+
+df4['Date'] = pd.to_datetime(df.Date, infer_datetime_format=True)
+
+# MM - Dropping rows with no date
+
+df4.dropna(subset=['Date'], inplace=True)
+print(df4)
+
+# MM - iterrows/Indexing to Add Year column
+
+import numpy as np
+
+df4['Year'] = np.nan
+
+for index, row in df4.iterrows():
+    df4.loc[index, 'Year'] = df.loc[index, 'Date'].year
+
+# MM - Data Cleansing
+
+df4['Year'] = df4['Year'].astype(int).replace('\.0', '', regex=True)
+df4['Volume'] = df4['Volume'].astype(int).replace('\.0', '', regex=True)
+df4.Open = df4.Open.round(2)
+df4.High = df4.High.round(2)
+df4.Low = df4.Low.round(2)
+df4.Close = df4.Close.round(2)
+df4['Adj Close'] = df4['Adj Close'].round(2)
